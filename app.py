34bfd0d2ea35692992493b5f9d92ee08c30e7029
Anas Wassan <72687368+anasaliw@users.noreply.github.com>
2024-08-07 22:04:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+account=os.getenv("ACCOUNT_SID")
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello World!'
+	return f'Hello World! {account}'
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
